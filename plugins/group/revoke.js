72cd8ebd019c47a535676d9cc7f07a871d997d8e
sbyuxD ! <irmalove999@gmail.com>
2026-08-17 15:32:11 +0700

    upload: plugins/group/revoke.js

---
+++ plugins/group/revoke.js
@@ -0,0 +1,16 @@
+export default {
+  name: "revoke",
+  category: "group",
+  command: ["revoke", "resetlink"],
+  settings: {
+    group: true,
+    admin: true,
+    botAdmin: true,
+  },
+  run: async (conn, m) => {
+    m.reply(
+      "Berhasil Reset linkgc\n\n Link : https://chat.whatsapp.com/" +
+        (await conn.groupRevokeInvite(m.chat)),
+    );
+  },
+};
